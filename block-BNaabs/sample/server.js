1d9b0ef05c239ce4e0f679384211adcf55eb1f54
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-07-11 20:09:14 +0530

    Basic Routing

--- block-BNaabs/sample/server.js
+++ block-BNaabs/sample/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
