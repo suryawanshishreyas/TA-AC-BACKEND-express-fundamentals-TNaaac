85cb0264c7609155bcc99d03439c4ae0ac7d665f
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-07-13 00:11:32 +0530

    assignment 3

---
+++ block-BNaabx/sample/server.js
@@ -0,0 +1,11 @@
+var express =require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(logger('dev'));
+app.use(cookieParser());
+
+app.listen(3000, ()=>{
+    console.log(`Server is listening on port 3000`);
+})
